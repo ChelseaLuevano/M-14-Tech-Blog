1fc2652e931102cfcc3f9b410d7d75e0575acae6
Chelsea-Luevano <120981491+ChelseaLuevano@users.noreply.github.com>
2023-05-13 13:17:19 -0400

    created nav bar rough draft and filled out GET routes pseudocode I will need for the nav; filled out the homeRoutes.js file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
+const apiRoutes = require("./api");
+const homeRoutes = require('./homeRoutes');
 
-router.use('/users', userRoutes);
+router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,13 @@
 const router = require('express').Router();
 
+
+
+// GET route for home page
+
+
+// GET route for dashboard page
+
+
+// GET route for login page
+
 module.exports = router;
